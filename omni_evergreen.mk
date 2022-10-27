186bf372b7678ce044d85e9577d05de5d8cbc131
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2022-10-27 18:02:42 +0000

    evergreen: bringup for twrp-12

--- omni_evergreen.mk
+++ omni_evergreen.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from evergreen device
 $(call inherit-product, device/xiaomi/evergreen/device.mk)
 
 PRODUCT_DEVICE := evergreen
-PRODUCT_NAME := omni_evergreen
+PRODUCT_NAME := twrp_evergreen
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := evergreen
 PRODUCT_MANUFACTURER := xiaomi
